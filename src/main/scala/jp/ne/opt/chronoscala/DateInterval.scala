31f5b5f7ac650abf5faf1433f73862786f89b33f
pishen tsai <pishen02@gmail.com>
2016-08-29 18:46:55 +0800

    add DateInterval

---
+++ src/main/scala/jp/ne/opt/chronoscala/DateInterval.scala
@@ -0,0 +1,23 @@
+package jp.ne.opt.chronoscala
+
+import java.time.{LocalDate, Period}
+import jp.ne.opt.chronoscala.Imports.richLocalDate
+
+/**
+ * Represents an immutable date interval
+ */
+case class DateInterval(startDate: LocalDate, endDate: LocalDate, step: Period) extends Seq[LocalDate] {
+  def apply(idx: Int) = {
+    if (0 <= idx && idx < length) {
+      iterator.drop(idx).next
+    } else {
+      throw new IndexOutOfBoundsException()
+    }
+  }
+  
+  def iterator = Iterator.iterate(startDate)(_ + step).takeWhile(_ <= endDate)
+  
+  def length = iterator.length
+
+  def by(step: Period) = this.copy(step = step)
+}
