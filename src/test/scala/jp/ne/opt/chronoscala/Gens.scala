4dc6c9007f31a683ba012633f8ac316cf15fea42
sh0hei <vividbit@gmail.com>
2019-02-24 18:28:28 +0900

    Add RichLocalTimeSpec

--- src/test/scala/jp/ne/opt/chronoscala/Gens.scala
+++ src/test/scala/jp/ne/opt/chronoscala/Gens.scala
@@ -7,10 +7,20 @@ import org.scalacheck.Gen
 trait Gens {
   def instantGen: Gen[Instant] = Gen.chooseNum(0L, Long.MaxValue).map(Instant.ofEpochMilli)
 
+  def hourGen: Gen[Int] = Gen.chooseNum(0, 23)
+  def minuteGen: Gen[Int] = Gen.chooseNum(0, 59)
+  def secondGen: Gen[Int] = Gen.chooseNum(0, 59)
+
   def localDateTimeGen: Gen[LocalDateTime] = for {
     instant <- instantGen
     zoneId <- Gen.oneOf(TimeZone.getAvailableIDs.map(TimeZone.getTimeZone(_).toZoneId).toSeq)
   } yield LocalDateTime.ofInstant(instant, zoneId)
+
+  def localTimeGen: Gen[LocalTime] = for {
+    hour <- hourGen
+    minute <- minuteGen
+    second <- secondGen
+  } yield LocalTime.of(hour, minute, second)
 
   def zonedDateTimeGen: Gen[ZonedDateTime] = for {
     instant <- instantGen
